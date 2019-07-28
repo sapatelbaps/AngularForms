20b15e038dbb6e4fbbca295d2fe74e30d88034b9
sapatelbaps <sapatelbaps@gmail.com>
2019-07-27 21:38:00 -0700

    Tried to implement ng-submit and ng-click but not working.

--- AngularForms/AngularForms/app/EmployeeForm/efController.js
+++ AngularForms/AngularForms/app/EmployeeForm/efController.js
@@ -1,5 +1,18 @@
 ﻿angularFormsApp.controller('efController',
     function efController($scope, efService) {
         $scope.employee = efService.employee;
+
+        $scope.departments = [
+            "Engineering",
+            "Marketing",
+            "Finance",
+            "Administration",
+            "Computer"];
+
+        $scope.submitForm = function () {
+            // If you put a breakpoint then this function will be called from the html component when submitted from the html tag at top or input tag at bottom.
+            debugger;
+
+        }
     }
 );
