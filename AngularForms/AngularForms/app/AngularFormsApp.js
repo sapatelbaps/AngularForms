dc92c0a3041bc290127f95ec10a4a910290eb6d0
sapatelbaps <sapatelbaps@gmail.com>
2019-07-28 01:50:00 -0700

    Removed the angular component from index page and will move to develop SPA application. Routing also added in the AngularFormsApp.js file. Not working as expected issue with course video. sample code given and things shown in the video are not same!

--- AngularForms/AngularForms/app/AngularFormsApp.js
+++ AngularForms/AngularForms/app/AngularFormsApp.js
@@ -1,1 +1,25 @@
-﻿var angularFormsApp = angular.module('angularFormsApp', []);
+﻿var angularFormsApp = angular.module('angularFormsApp', ["ngRoute"]);
+
+angularFormsApp.config(function ($routeProvider) {
+    $routeProvider.when(
+            "/home",
+            {
+                templateUrl: "app/Home.html",
+                controller: "HomeController"
+            })
+        .when(
+            "/newEmployeeForm",
+            {
+                templateUrl: "app/EmployeeForm/efTemplate.html",
+                controller: "efController"
+            })
+        .otherwise({ redirectTo: "/home" });
+});
+
+angularFormsApp.config("HomeController",
+    function ($scope, $location) {
+
+        $scope.addNewEmployee = function () {
+            $location.path('/newEmployeeForm');
+        }
+    });
